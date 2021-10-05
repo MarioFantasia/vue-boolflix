64842d7c57d22327bcb1cc2caeadfe2bc653dd53
MarioFantasia <mariofantasia18@outlook.it>
2021-10-05 16:47:15 +0200

    ricerca dati in console

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,26 @@
 <script>
 import axios from 'axios';
 import Header from './components/Header.vue';
-import Films from './components/Films.vue';
+import Films from './components/Films.vue'; 
 export default {
     components : {
       Header,
       Films
     },
     created() {
-      axios.get('https://api.themoviedb.org/3/search/movie'), {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
         params : {
-          
+          api_key : '4fb27bad81fd725bbb6d36e4ce533e33',
+          query : 'Ritorno al futuro',
+          language : 'it-IT'
         }
+      })
+      .then(function(res) {
+        console.log(res.data.results); //cerco i dati in console
       }
+      
+      );
+
     }
 }
 </script>
@@ -27,3 +35,5 @@ export default {
 <style lang="scss">
 
 </style>
+
+
