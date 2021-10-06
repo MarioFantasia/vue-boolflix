c78494f8e18b928014f0adaca92cd503a3df61cb
MarioFantasia <mariofantasia18@outlook.it>
2021-10-06 18:28:02 +0200

    aggiunta bandiere serie

--- src/components/Serie.vue
+++ src/components/Serie.vue
@@ -2,7 +2,7 @@
     <li>
         <h3>{{objectSerie.name}}</h3>
         <h3>{{objectSerie.original_name}}</h3>
-        <h4>{{objectSerie.original_language}}</h4>
+        <img style="width:25px" :src="'https://www.unknown.nu/flags/images/' + objectSerie.original_language + '-100'" :alt="objectSerie.original_language">
         <h5>{{objectSerie.vote_average}}</h5>
     </li>
 </template>
