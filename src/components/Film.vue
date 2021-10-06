67838f810fbd260c2bb18bdb4984327983d10446
MarioFantasia <mariofantasia18@outlook.it>
2021-10-06 17:28:56 +0200

    aggiunta bandiere

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -2,7 +2,8 @@
         <li>
             <h3>{{objectFilm.title}}</h3>
             <h3>{{objectFilm.original_title}}</h3>
-            <h4>{{objectFilm.original_language}}</h4>
+            <!-- <h4>{{objectFilm.original_language}}</h4> -->
+            <img :src="'https://www.unknown.nu/flags/images/' + objectFilm.original_language + '-100'" :alt="objectFilm.original_language">
             <h5>{{objectFilm.vote_average}}</h5>
         </li>
 </template>
