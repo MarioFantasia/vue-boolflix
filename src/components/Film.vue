c241ea02ede637e75ea535030fb8b68fc65189b2
MarioFantasia <mariofantasia18@outlook.it>
2021-10-07 12:47:09 +0200

    aggiunta copertine film e serie

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -2,6 +2,8 @@
         <li>
             <h3>{{objectFilm.title}}</h3>
             <h3>{{objectFilm.original_title}}</h3>
+            <img v-if="objectFilm.poster_path" :src="'https://image.tmdb.org/t/p/w342/' + objectFilm.poster_path" alt="Img">
+            <img v-else src="../assets/img/imageNoPresent.png" alt="Img" style="width:342px">
             <img style="width:25px" :src="'https://www.unknown.nu/flags/images/' + objectFilm.original_language + '-100'" :alt="objectFilm.original_language">
             <h5>{{objectFilm.vote_average}}</h5>
         </li>
@@ -17,5 +19,8 @@ export default {
 </script>
 
 <style lang="scss" scoped> 
-
+    img {
+        display: block;     //temporanee
+        margin-bottom: 5px; 
+    }
 </style>
--- src/components/Serie.vue
+++ src/components/Serie.vue
@@ -2,6 +2,8 @@
     <li>
         <h3>{{objectSerie.name}}</h3>
         <h3>{{objectSerie.original_name}}</h3>
+        <img v-if="objectSerie.poster_path" :src="'https://image.tmdb.org/t/p/w342/' + objectSerie.poster_path" alt="Img">
+            <img v-else src="../assets/img/imageNoPresent.png" alt="Img" style="width:342px">
         <img style="width:25px" :src="'https://www.unknown.nu/flags/images/' + objectSerie.original_language + '-100'" :alt="objectSerie.original_language">
         <h5>{{objectSerie.vote_average}}</h5>
     </li>
@@ -17,5 +19,8 @@ export default {
 </script>
 
 <style lang="scss" scoped> 
-
+    img {
+        display: block;     //temporanee
+        margin-bottom: 5px; 
+    }
 </style>
