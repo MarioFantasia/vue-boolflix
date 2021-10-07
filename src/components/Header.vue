9732277d4098316403e9e7bda9920b2cb81f76e9
MarioFantasia <mariofantasia18@outlook.it>
2021-10-07 22:43:26 +0200

    stile header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,16 @@
 <template>
-    <header>
-        <div class="search">
-            <input type="text" name="" id="" placeholder="Cerca film o serie TV"
-            v-model="searchItem">
+    <header class="flex">
+        <div class="logo flex">
+            <div>BOOLFLIX</div>
         </div>
-        <div class="button">
-            <button @click.prevent="$emit('search', searchItem)">Cerca</button>
+        <div class="search flex">
+            <div>
+                <input type="text" name="" id="" placeholder="Cerca film o serie TV"
+                v-model="searchItem">
+            </div>
+            <div>
+                <button @click.prevent="$emit('search', searchItem)">Cerca</button>
+            </div>
         </div>
     </header>
 </template>
@@ -22,5 +27,24 @@ export default {
 </script>
 
 <style lang="scss" scoped> 
-
+    header {
+        padding: 10px;
+        justify-content: space-between;
+        background-color: black;
+        opacity: 0.8;
+        .logo {
+            font-size: 30px;
+            font-weight: 800;
+            color: red;
+        }
+        .search {
+            div {
+                margin: auto 0;
+            }
+            input {
+                margin-right: 5px;
+                outline: none;
+            }
+        }
+    }
 </style>
