55f1545570cf07f1215982749f0687bb0c65d425
MarioFantasia <mariofantasia18@outlook.it>
2021-10-07 15:24:49 +0200

    voto serie

--- src/components/Serie.vue
+++ src/components/Serie.vue
@@ -5,7 +5,7 @@
         <img v-if="objectSerie.poster_path" :src="'https://image.tmdb.org/t/p/w342/' + objectSerie.poster_path" alt="Img">
             <img v-else src="../assets/img/imageNoPresent.png" alt="Img" style="width:342px">
         <img style="width:25px" :src="'https://www.unknown.nu/flags/images/' + objectSerie.original_language + '-100'" :alt="objectSerie.original_language">
-        <h5>{{objectSerie.vote_average}}</h5>
+        <!-- <h5>{{objectSerie.vote_average}}</h5> -->
         <h5>{{this.vote}}</h5>
     </li>
 </template>
